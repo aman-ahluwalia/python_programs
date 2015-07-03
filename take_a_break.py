5e318eb0b4c69d7b51fea45f32c554e74e033541
Aman <aman@Amans-MacBook-Air.local>
2015-07-03 13:21:41 +0530

    Movie website added.

--- take_a_break.py
+++ take_a_break.py
@@ -6,6 +6,6 @@ break_count = 0
 
 print("This program started at " + time.ctime())
 while (break_count < total_breaks):
-	time.sleep(10)
+	time.sleep(2*60) //wait time specifying seconds to wait, here it is 2 minutes
 	webbrowser.open("http://www.facebook.com/")
 	break_count = break_count + 1
